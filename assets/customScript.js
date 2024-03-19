b9c7ba5c07f5813e822ae39a75b462e2c684fdc5
Saad Ullah Sajid <saad.sajid@c2-digital.com>
2024-03-19 18:14:11 +0500

    Tabs Fixed

--- assets/customScript.js
+++ assets/customScript.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
     $(".popupBtn").click(function () {
         $("body, html").addClass("enableScroll");
     });
+    $(".tabHeading").click(function(){
+       $(this).toggleClass("active");
+       $(this).next(".tabContent ").slideToggle(300);
+    });
 });
